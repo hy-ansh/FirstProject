c699bbb76b2d464bcee7a60f67940ebb80091074
Ansh <hpannsh@gmail.com>
2024-09-30 01:23:38 +0530

    minimal changes

--- java.js
+++ java.js
@@ -8,22 +8,33 @@ let conpas = document.querySelector("#conpas");
 let mobile = document.querySelector("#mob");
 let pass = document.querySelector("#pass");
 let box = document.querySelector(".box")
-let fp = document.querySelector("#para");
+let p = document.querySelector("p");
+let form=document.querySelector(".form");
+
+p.style.display="none";
 signinbtn.onclick = ()=>{
     title.innerHTML="LOGIN";
-    mobile.style.position="absolute";
-    name.style.position="absolute";
-    conpas.style.position="absolute";
-    box.style.height ="500px";
+    // mobile.style.position="absolute";
+    // name.style.position="absolute";
+    // conpas.style.position="absolute";
+    box.style.height ="400px";
     signupbtn.classList.add("disable");
     signinbtn.classList.remove("disable");
+    name.style.display="none";
+    email.style.display="none";
+    conpas.style.display="none";
+    p.style.display="inline";
+    
    
 }
 signupbtn.onclick = function (){
     title.innerText = "Sign Up";
     mobile.style.position = "relative";
     box.style.height ="600px";
- 
+    p.style.display="none";
     signinbtn.classList.add("disable");
     signupbtn.classList.remove("disable");
+    name.style.display="block";
+    email.style.display="block";
+    conpas.style.display="block";
 }
